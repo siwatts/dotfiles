8373c41b1f823cb2c23538a9d9e29b2d0ad87e1e
Simon Watts <siwatts15@outlook.com>
2018-05-06 18:19:48 +0100

    Script to get current volume.

---
+++ bin/print_vol.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Get the current volume percentage of the first output sink.
+SINK=0
+pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1%,'
