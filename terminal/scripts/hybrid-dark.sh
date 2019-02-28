74c5b10f4a6c6235df680c4c562cb71a4c4351d0
Simon Watts <siwatts15@outlook.com>
2019-02-28 20:44:50 +0000

    Hybrid dark terminal theme.

---
+++ terminal/scripts/hybrid-dark.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+# base16-shell (https://github.com/chriskempson/base16-shell)
+# Base16 Shell template by Chris Kempson (http://chriskempson.com)
+# Shell script generated from file hybrid-dark.Xresources
+
+color_foreground="C5/C8/C6"
+color_background="1D/1F/21"
+color00="28/2A/2E"
+color08="37/3B/41"
+color01="A5/42/42"
+color09="CC/66/66"
+color02="8C/94/40"
+color10="B5/BD/68"
+color03="DE/93/5F"
+color11="F0/C6/74"
+color04="5F/81/9D"
+color12="81/A2/BE"
+color05="85/67/8F"
+color13="B2/94/BB"
+color06="5E/8D/87"
+color14="8A/BE/B7"
+color07="70/78/80"
+color15="C5/C8/C6"
+# Restore colors 16-21 to pre-base16 values.
+color16="00/00/00"
+color17="00/00/5f"
+color18="00/00/87"
+color19="00/00/af"
+color20="00/00/d7"
+color21="00/00/ff"
+
+if [ -n "$TMUX" ]; then
+  # Tell tmux to pass the escape sequences through
+  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
+elif [ "${TERM%%[-.]*}" = "screen" ]; then
+  # GNU screen (screen, screen-256color, screen-256color-bce)
+  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
+  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
+  put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
+elif [ "${TERM%%-*}" = "linux" ]; then
+  put_template() { [ $1 -lt 16 ] && printf "\e]P%x%s" $1 $(echo $2 | sed 's/\///g'); }
+  put_template_var() { true; }
+  put_template_custom() { true; }
+else
+  put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
+  put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
+  put_template_custom() { printf '\033]%s%s\033\\' $@; }
+fi
+
+# 16 color space
+put_template 0  $color00
+put_template 1  $color01
+put_template 2  $color02
+put_template 3  $color03
+put_template 4  $color04
+put_template 5  $color05
+put_template 6  $color06
+put_template 7  $color07
+put_template 8  $color08
+put_template 9  $color09
+put_template 10 $color10
+put_template 11 $color11
+put_template 12 $color12
+put_template 13 $color13
+put_template 14 $color14
+put_template 15 $color15
+
+# 256 color space
+put_template 16 $color16
+put_template 17 $color17
+put_template 18 $color18
+put_template 19 $color19
+put_template 20 $color20
+put_template 21 $color21
+
+# foreground / background / cursor color
+if [ -n "$ITERM_SESSION_ID" ]; then
+  # iTerm2 proprietary escape codes
+  put_template_custom Pg e9e9f4 # foreground
+  put_template_custom Ph 282936 # background
+  put_template_custom Pi e9e9f4 # bold color
+  put_template_custom Pj 4d4f68 # selection color
+  put_template_custom Pk e9e9f4 # selected text color
+  put_template_custom Pl e9e9f4 # cursor
+  put_template_custom Pm 282936 # cursor text
+else
+  put_template_var 10 $color_foreground
+  if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]; then
+    put_template_var 11 $color_background
+    if [ "${TERM%%-*}" = "rxvt" ]; then
+      put_template_var 708 $color_background # internal border (rxvt)
+    fi
+  fi
+  #put_template_custom 12 ";7" # cursor (reverse video)
+  #put_template_custom 12 ";#ff66ff" # cursor (reverse video)
+fi
+
+# clean up
+unset -f put_template
+unset -f put_template_var
+unset -f put_template_custom
+unset color00
+unset color01
+unset color02
+unset color03
+unset color04
+unset color05
+unset color06
+unset color07
+unset color08
+unset color09
+unset color10
+unset color11
+unset color12
+unset color13
+unset color14
+unset color15
+unset color16
+unset color17
+unset color18
+unset color19
+unset color20
+unset color21
+unset color_foreground
+unset color_background
+
