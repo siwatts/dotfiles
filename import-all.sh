aaafcb7a94958ac58c9760bf868f705b9d60c9da
Simon Watts <siwatts15@outlook.com>
2022-11-12 21:38:11 +0000

    Live dnf upgrade option to import script (for VMs)

--- import-all.sh
+++ import-all.sh
@@ -54,7 +54,16 @@ echo "It is HIGHLY RECOMMENDED that you dnf upgrade all packages and reboot befo
 read -r -p 'DNF upgrade all packages and reboot now? (y/[N]): ' response
 case "$response" in
     [yY][eE][sS]|[yY])
-        echo "Working..."; sudo dnf offline-upgrade download -y && echo "Initiating REBOOT, 1 minute from $(date). Save and close all work." && sleep 1m && sudo dnf offline-upgrade reboot
+        echo "Using DNF offline-upgrade plugin can be more safe on a physical machine, but is not pre-installed on some systems like the KDE spin and can cause issues in some VMs"
+        read -r -p 'Use more risky fallback live dnf upgrade in current login session? (recommended for VMs)? (y/[N]): ' response
+        case "$response" in
+            [yY][eE][sS]|[yY])
+                echo "Working..."; sudo dnf upgrade -y && echo "Initiating REBOOT, 1 minute from $(date). Save and close all work." && sudo shutdown -r 1
+                ;;
+            *)
+                echo "Working..."; sudo dnf offline-upgrade download -y && echo "Initiating REBOOT, 1 minute from $(date). Save and close all work." && sleep 1m && sudo dnf offline-upgrade reboot
+                ;;
+        esac
         ;;
     *)
         ;;
