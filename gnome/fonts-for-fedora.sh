bf8757c8bb456405dacabc47466fafdab545abef
Simon Watts <siwatts15@outlook.com>
2024-03-13 20:34:50 +0000

    Add latest GNOME configuration (F39)
    - Arc Menu from Super to Super+X
    - Nightlight off
    - Nautilus preferences
    - Software auto-updates off
    - Modals un-attached
    - Gnome terminal as default terminal keybinding and pinned app
    - More misc. config

--- gnome/fonts-for-fedora.sh
+++ gnome/fonts-for-fedora.sh
@@ -24,38 +24,38 @@ else
     HACKFONT=no
 fi
 
-# Set our new preferred default, currently IBM Plex Mono
-if [ "$CUSTOMFONT" == "yes" ]; then
-    # Equivalent to `dconf load /` of a file containing [org/gnome/desktop/interface] monospace-font-name=
-    echo "GNOME..."
-    gsettings set org.gnome.desktop.interface monospace-font-name 'IBM Plex Mono 10'
-
-    # GVim (font was previously 'Source Code Pro')
-    if [ -f ~/.vimrc ]; then
-        echo "GVim..."
-        sed -i 's/\(\s*\)set guifont=Source/\1"set guifont=Source/g' ~/.vimrc
-        sed -i 's/\(\s*\)"set guifont=IBM/\1set guifont=IBM/g' ~/.vimrc
-    else
-        echo "GVim... '~/.vimrc' not found"
-    fi
-
-    # neovim-qt
-    if [ -f ~/.config/nvim/ginit.vim ]; then
-        echo "neovim-qt..."
-        sed -i 's/^GuiFont Source/"GuiFont Source/g' ~/.config/nvim/ginit.vim
-        sed -i 's/^"GuiFont IBM/GuiFont IBM/g' ~/.config/nvim/ginit.vim
-    else
-        echo "neovim-qt... '~/.config/nvim/ginit.vim' not found"
-    fi
-
-    # Doom Emacs
-    if [ -f ~/.doom.d/config.el ]; then
-        echo "Doom Emacs..."
-        sed -i 's/Source Code Pro/IBM Plex Mono/g' ~/.doom.d/config.el
-    else
-        echo "Doom Emacs... '~/.doom.d/config.el' not found"
-    fi
-
-    echo "Font setting complete"
-fi
+## Set our new preferred default, currently IBM Plex Mono
+#if [ "$CUSTOMFONT" == "yes" ]; then
+#    # Equivalent to `dconf load /` of a file containing [org/gnome/desktop/interface] monospace-font-name=
+#    echo "GNOME..."
+#    gsettings set org.gnome.desktop.interface monospace-font-name 'IBM Plex Mono 10'
+#
+#    # GVim (font was previously 'Source Code Pro')
+#    if [ -f ~/.vimrc ]; then
+#        echo "GVim..."
+#        sed -i 's/\(\s*\)set guifont=Source/\1"set guifont=Source/g' ~/.vimrc
+#        sed -i 's/\(\s*\)"set guifont=IBM/\1set guifont=IBM/g' ~/.vimrc
+#    else
+#        echo "GVim... '~/.vimrc' not found"
+#    fi
+#
+#    # neovim-qt
+#    if [ -f ~/.config/nvim/ginit.vim ]; then
+#        echo "neovim-qt..."
+#        sed -i 's/^GuiFont Source/"GuiFont Source/g' ~/.config/nvim/ginit.vim
+#        sed -i 's/^"GuiFont IBM/GuiFont IBM/g' ~/.config/nvim/ginit.vim
+#    else
+#        echo "neovim-qt... '~/.config/nvim/ginit.vim' not found"
+#    fi
+#
+#    # Doom Emacs
+#    if [ -f ~/.doom.d/config.el ]; then
+#        echo "Doom Emacs..."
+#        sed -i 's/Source Code Pro/IBM Plex Mono/g' ~/.doom.d/config.el
+#    else
+#        echo "Doom Emacs... '~/.doom.d/config.el' not found"
+#    fi
+#
+#    echo "Font setting complete"
+#fi
 
